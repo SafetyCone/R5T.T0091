fa49d36c479b7dcb8d100865ee05335237c69bda
davidcoats <david@getsafetycone.com>
2021-11-22 11:33:44 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0091.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0091.Construction
+{
+    /// <summary>
+    /// Construction project for IMachineMessage interface definition and common, public types..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0091/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0091
+{
+    /// <summary>
+    /// IMachineMessage interface definition and common, public types.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
